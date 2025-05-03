96895a0661c66d1ff8550db9f4ec72d9f986c657
haloapping <alfiyanto.kondolele@gmail.com>
2025-05-03 19:57:40 +0700

    feat: add basepath route, prisma client and openapi

---
+++ src/configs/app.ts
@@ -0,0 +1,16 @@
+export const configGeneral = {
+  title: "Lyrifix API",
+  description: "Fix the Lyrics, Feel the Music",
+  urls: [
+    {
+      name: "GitHub",
+      url: "https://github.com/lyrifix/lyrifix-api",
+    },
+  ],
+};
+
+export const configDocs = {
+  pageTitle: "Lyrifix API",
+  docs: "/docs",
+  openapi: "/openapi.json",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { configDocs, configGeneral } from "./configs/app";
 
-const app = new Hono()
+const app = new OpenAPIHono();
+app.use(cors());
+app.use(logger());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const apiRoutes = app.basePath("/");
 
-export default app
+apiRoutes
+  .doc(configDocs.openapi, {
+    openapi: "3.1.0",
+    info: { ...configGeneral, version: "v1" },
+  })
+  .get(
+    "/",
+    Scalar({
+      pageTitle: "Lyrifix",
+      url: "/openapi.json",
+    })
+  )
+  .onError((err, c) => {
+    return c.json({ code: 400, status: "error", message: err.message }, 400);
+  });
+
+export default app;
