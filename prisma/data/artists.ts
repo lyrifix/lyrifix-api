b68d430bbbbc46013d1525965434f0d89cb2fab6
indahmutiah <indahmutiahmz@gmail.com>
2025-05-10 12:20:17 +0700

    feat: add songs, lyrics, and artist

--- prisma/data/artists.ts
+++ prisma/data/artists.ts
@@ -4,7 +4,8 @@ export const dataArtists: CreateArtistType[] = [
   {
     slug: "taylor-swift",
     name: "Taylor Swift",
-    imageUrl: "https://ucarecdn.com/c8287ba7-11d7-4230-b1c6-f3ff076c3e4e/taylorswift.png",
+    imageUrl:
+      "https://ucarecdn.com/c8287ba7-11d7-4230-b1c6-f3ff076c3e4e/taylorswift.png",
   },
   {
     slug: "wr-soepratman",
@@ -15,26 +16,37 @@ export const dataArtists: CreateArtistType[] = [
   {
     slug: "hayashi-hiromori",
     name: "Hayashi Hiromori",
-    imageUrl: "https://ucarecdn.com/488b290a-03ff-4247-be72-0fc1cc0c2f34/hayashihiromori.jpg",
+    imageUrl:
+      "https://ucarecdn.com/488b290a-03ff-4247-be72-0fc1cc0c2f34/hayashihiromori.jpg",
   },
   {
     slug: "perunggu",
     name: "Perunggu",
-    imageUrl: "https://ucarecdn.com/6a4aad1b-b0b1-4e0a-9208-1e45456c5e42/perunggu.webp",
+    imageUrl:
+      "https://ucarecdn.com/6a4aad1b-b0b1-4e0a-9208-1e45456c5e42/perunggu.webp",
   },
   {
     slug: "elephant-kind",
     name: "Elephand Kind",
-    imageUrl: "https://ucarecdn.com/bd120b96-445e-4a9a-91ec-9ae52e0982a8/elephantkind.webp",
+    imageUrl:
+      "https://ucarecdn.com/bd120b96-445e-4a9a-91ec-9ae52e0982a8/elephantkind.webp",
   },
   {
     slug: "foo-fighters",
     name: "Foo Fighters",
-    imageUrl: "https://ucarecdn.com/609066e4-072e-4522-9a73-2a6e05c83be4/foofighters.webp",
+    imageUrl:
+      "https://ucarecdn.com/609066e4-072e-4522-9a73-2a6e05c83be4/foofighters.webp",
   },
   {
     slug: "john-mayer",
     name: "John Mayer",
-    imageUrl: "https://ucarecdn.com/fc752d3b-dcc7-4d16-a422-678e328a7762/johnmayer.webp",
+    imageUrl:
+      "https://ucarecdn.com/fc752d3b-dcc7-4d16-a422-678e328a7762/johnmayer.webp",
+  },
+  {
+    slug: "ariana-grande",
+    name: "Ariana Grande",
+    imageUrl:
+      "https://ucarecdn.com/6b516d87-0cbb-4c0e-9018-c871e0e76ef9/arianagrande.webp",
   },
 ];
---
+++ prisma/data/lyrics/supernatural.ts
@@ -0,0 +1,54 @@
+const lyricText = `
+Don't want nobody else around me
+Just need you right here
+You're like the only thing that I see
+It's crystal-clear, and I
+
+I want you to come claim it, I do
+What are you waiting for?
+Yeah, I want you to name it, I do
+Want you to make it yours
+
+It's like supernatural
+This love's possessin' me, but I don't mind at all
+It's like supernatural
+It's takin' over me, don't wanna fight the fall
+It's like supernatural
+No, I don't even mind at all
+It's like supernatural
+(This love's possessin' me, but I don't mind at all)
+
+Need your hands all up on my body
+Like the moon needs the stars (like the moon needs the stars)
+Nothin' else felt this way inside me
+Boy, let's go too far
+
+I want you to come claim it, I do
+What are you waitin' for? (Ooh)
+Yeah, want you to name it, I do
+Want you to make it yours
+
+It's like supernatural
+This love's possessin' me, but I don't mind at all
+It's like supernatural
+It's takin' over me, don't wanna fight the fall
+It's like supernatural
+No, I don't even mind at all
+It's like supernatural
+No, I don't even mind at all
+
+Yeah, mmm
+I want you to come claim it, I do
+I do, I do
+You to come name it, I do (do)
+
+This love's possessin' me, but I don't mind at all
+It's like supernatural
+It's takin' over me, don't wanna fight the fall
+It's like supernatural
+No, I don't even mind at all (ooh)
+It's like supernatural (ooh)
+No, I don't even mind at all
+It's like supernatural`;
+
+export default lyricText;
