02513f4b86019aa889ef485966655d1f269a939c
haloapping <alfiyanto.kondolele@gmail.com>
2025-05-10 10:10:56 +0700

    fix: store artist and song images to uploadcore

--- prisma/data/artists.ts
+++ prisma/data/artists.ts
@@ -4,17 +4,16 @@ export const dataArtists: CreateArtistType[] = [
   {
     slug: "taylor-swift",
     name: "Taylor Swift",
-    imageUrl: "https://ik.imagekit.io/y7wb3isgw/artists/taylor-swift.png",
+    imageUrl: "https://ucarecdn.com/c8287ba7-11d7-4230-b1c6-f3ff076c3e4e/taylorswift.png",
   },
   {
     slug: "wr-soepratman",
     name: "Wage Rudolf Soepratman",
-    imageUrl:
-      "https://ik.imagekit.io/y7wb3isgw/artists/wage-rudolf-supratman.jpg",
+    imageUrl: "https://ikhttps://ucarecdn.com/484889bf-2301-49b9-83dc-f4e235d73bf4/wagerudolfsupratman.jpg",
   },
   {
     slug: "hayashi-hiromori",
     name: "Hayashi Hiromori",
-    imageUrl: "https://ik.imagekit.io/y7wb3isgw/artists/hayashi-hiromori.jpg",
+    imageUrl: "https://ucarecdn.com/488b290a-03ff-4247-be72-0fc1cc0c2f34/hayashihiromori.jpg",
   },
 ];
