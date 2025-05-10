11b55a8cc6a7a7130fda6c10109cd43f74f7fe7c
Vigo Putra <artupogiv@gmail.com>
2025-05-10 11:36:24 +0700

    chore: update seed

--- prisma/data/artists.ts
+++ prisma/data/artists.ts
@@ -4,17 +4,37 @@ export const dataArtists: CreateArtistType[] = [
   {
     slug: "taylor-swift",
     name: "Taylor Swift",
-    imageUrl: "https://ik.imagekit.io/y7wb3isgw/artists/taylor-swift.png",
+    imageUrl: "https://ucarecdn.com/c8287ba7-11d7-4230-b1c6-f3ff076c3e4e/taylorswift.png",
   },
   {
     slug: "wr-soepratman",
     name: "Wage Rudolf Soepratman",
     imageUrl:
-      "https://ik.imagekit.io/y7wb3isgw/artists/wage-rudolf-supratman.jpg",
+      "https://ucarecdn.com/484889bf-2301-49b9-83dc-f4e235d73bf4/wagerudolfsupratman.jpg",
   },
   {
     slug: "hayashi-hiromori",
     name: "Hayashi Hiromori",
-    imageUrl: "https://ik.imagekit.io/y7wb3isgw/artists/hayashi-hiromori.jpg",
+    imageUrl: "https://ucarecdn.com/488b290a-03ff-4247-be72-0fc1cc0c2f34/hayashihiromori.jpg",
+  },
+  {
+    slug: "perunggu",
+    name: "Perunggu",
+    imageUrl: "https://ucarecdn.com/6a4aad1b-b0b1-4e0a-9208-1e45456c5e42/perunggu.webp",
+  },
+  {
+    slug: "elephant-kind",
+    name: "Elephand Kind",
+    imageUrl: "https://ucarecdn.com/bd120b96-445e-4a9a-91ec-9ae52e0982a8/elephantkind.webp",
+  },
+  {
+    slug: "foo-fighters",
+    name: "Foo Fighters",
+    imageUrl: "https://ucarecdn.com/609066e4-072e-4522-9a73-2a6e05c83be4/foofighters.webp",
+  },
+  {
+    slug: "john-mayer",
+    name: "John Mayer",
+    imageUrl: "https://ucarecdn.com/fc752d3b-dcc7-4d16-a422-678e328a7762/johnmayer.webp",
   },
 ];
--- prisma/data/lyrics.ts
+++ prisma/data/lyrics.ts
@@ -3,6 +3,13 @@ import { prepareText } from "../../src/lib/text";
 
 import lyricIndonesiaRaya from "./lyrics/indonesia-raya";
 import lyricKimigayo from "./lyrics/kimigayo";
+import lyricAntiHero from "./lyrics/anti-hero";
+import lyric33x from "./lyrics/33x";
+import lyricBetterDays from "./lyrics/better-days";
+import lyricMyHero from "./lyrics/my-hero";
+import lyricRencanaUsang from "./lyrics/rencana-usang";
+import lyricWhyGerogia from "./lyrics/why-georgia";
+
 
 export const dataLyrics: CreateLyricType[] = [
   {
@@ -18,6 +25,32 @@ export const dataLyrics: CreateLyricType[] = [
   {
     slug: "anti-hero-v1",
     songSlug: "anti-hero",
-    text: `I have this thing where I get older but just never wiser\nMidnights become my afternoons\nWhen my depression works the graveyard shift\nAll of the people I've ghosted stand there in the room\nI should not be left to my own devices\nThey come with prices and vices\nI end up in crisis (tale as old as time)\nI wake up screaming from dreaming\nOne day I'll watch as you're leaving\n'Cause you got tired of my scheming\n(For the last time)\nIt's me, hi, I'm the problem, it's me\nAt tea time, everybody agrees\nI'll stare directly at the sun but never in the mirror\nIt must be exhausting always rooting for the anti-hero\nSometimes I feel like everybody is a sexy baby\nAnd I'm a monster on the hill\nToo big to hang out, slowly lurching toward your favorite city\nPierced through the heart, but never killed\nDid you hear my covert narcissism I disguise as altruism\nLike some kind of congressman? (Tale as old as time)\nI wake up screaming from dreaming\nOne day I'll watch as you're leaving\nAnd life will lose all its meaning\n(For the last time)\nIt's me, hi, I'm the problem, it's me (I'm the problem, it's me)\nAt tea time, everybody agrees\nI'll stare directly at the sun but never in the mirror\nIt must be exhausting always rooting for the anti-hero\nI have this dream my daughter in-law kills me for the money\nShe thinks I left them in the will\nThe family gathers 'round and reads it and then someone screams out\n"She's laughing up at us from hell"\nIt's me, hi, I'm the problem, it's me\nIt's me, hi, I'm the problem, it's me\nIt'\s me, hi, everybody agrees, everybody agrees\nIt'\s me, hi (hi), I'm the problem, it's me (I'm the problem, it's me)\nAt tea (tea) time (time), everybody agrees (everybody agrees)\nI'll stare directly at the sun but never in the mirror\nIt must be exhausting always rooting for the anti-hero\n`,
+    text: prepareText(lyricAntiHero),
   },
+  {
+    slug: "33x-v1",
+    songSlug: "33x",
+    text: prepareText(lyric33x),
+  },
+  {
+    slug: "better-days-v1",
+    songSlug: "better-days",
+    text: prepareText(lyricBetterDays),
+  },
+  {
+    slug: "my-hero-v1",
+    songSlug: "my-hero",
+    text: prepareText(lyricMyHero),
+  },
+  {
+    slug: "rencana-usang-v1",
+    songSlug: "rencana-usang",
+    text: prepareText(lyricRencanaUsang),
+  },
+  {
+    slug: "why-georgia-v1",
+    songSlug: "why-georgia",
+    text: prepareText(lyricWhyGerogia),
+  },
+  
 ];
