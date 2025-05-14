4730eba096defec8cba47f68c5dad4568430b1c7
haloapping <alfiyanto.kondolele@gmail.com>
2025-05-14 22:12:42 +0700

    feat: add endpoint lyric

--- src/schema/lyric.ts
+++ src/schema/lyric.ts
@@ -3,20 +3,30 @@ import { z } from "@hono/zod-openapi";
 export const LyricSchema = z.object({
   id: z.string().ulid(),
   slug: z.string(),
+  text: z.string().min(10),
   songId: z.string(),
-  text: z.string(),
   createdAt: z.string().datetime().optional(),
   updatedAt: z.string().datetime().optional().nullable(),
 });
 
-export const CreateLyricSchema = LyricSchema.extend({
-  id: z.string().ulid().optional(),
-  songSlug: z.string(),
-}).omit({
-  songId: true,
+export const CreateLyricSchema = LyricSchema.omit({
+  id: true,
+  slug: true,
   createdAt: true,
   updatedAt: true,
 });
 
+export const UpdateLyricSchema = LyricSchema.omit({
+  id: true,
+  songId: true,
+  slug: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const LyricsSchema = z.array(LyricSchema);
+
 export type LyricType = z.infer<typeof LyricSchema>;
+export type LyricsType = z.infer<typeof LyricsSchema>;
 export type CreateLyricType = z.infer<typeof CreateLyricSchema>;
+export type UpdateLyricType = z.infer<typeof UpdateLyricSchema>;
